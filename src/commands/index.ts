49ef9d083fbdbed140c67b6bdddfa300395fa825
Yael Eduardo Ibarra Garduño <ibgayael@gmail.com>
2024-05-27 07:18:10 -0600

    Removed test commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,13 +1,7 @@
-import * as chungus from './utility/chungus'
-import * as user from './utility/user'
-import * as server from './utility/server'
 import * as play from './music/play'
 import * as skip from './music/skip'
 
 export default {
-    chungus,
-    user,
-    server,
     play,
     skip
 }
